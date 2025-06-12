bda688198a0515ec0888eae7c3133036e1ffb4cb
Art Genricx Alejandria <artalejandria@gmail.com>
2025-06-12 22:11:30 +0800

    Update RegistrationPage.jsx

--- alejandria-front-end/src/pages/RegistrationPage.jsx
+++ alejandria-front-end/src/pages/RegistrationPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import Button from '../components/Button';
-import { createUser } from '../services/userService'; 
+import { createUser } from '../services/UserService'; 
 import '../styles/Regis.css'; 
 
 function RegisterPage() {
