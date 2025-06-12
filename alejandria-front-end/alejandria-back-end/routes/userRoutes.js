180b445870f2a862185106ad24b4b1a6a428e822
Art Genricx Alejandria <artalejandria@gmail.com>
2025-06-12 22:49:51 +0800

    Update userRoutes.js

--- alejandria-front-end/alejandria-back-end/routes/userRoutes.js
+++ alejandria-front-end/alejandria-back-end/routes/userRoutes.js
@@ -1,10 +1,22 @@
 const express = require('express');
-const { getUsers, createUser, updateUser, deleteUser, loginUser } = require('../controllers/userController');
-
 const router = express.Router();
 
-router.route('/').get(getUsers).post(createUser);
-router.route('/:id').put(updateUser).delete(deleteUser);
-router.post('/login', loginUser);
+// Simple test route
+router.get('/test', (req, res) => {
+  res.json({ message: 'User routes working!' });
+});
+
+// Basic routes without complex patterns
+router.get('/', (req, res) => {
+  res.json({ message: 'Get all users - placeholder' });
+});
+
+router.post('/', (req, res) => {
+  res.json({ message: 'Create user - placeholder' });
+});
+
+router.post('/login', (req, res) => {
+  res.json({ message: 'Login - placeholder' });
+});
 
 module.exports = router;
