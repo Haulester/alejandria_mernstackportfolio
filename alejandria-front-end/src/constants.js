af6ee1b89cc7824be39a62055f625ce4a9bc3ab7
Art Genricx Alejandria <artalejandria@gmail.com>
2025-06-12 23:15:41 +0800

    Update constants.js

--- alejandria-front-end/src/constants.js
+++ alejandria-front-end/src/constants.js
@@ -1,5 +1,6 @@
+// Updated for production - June 12, 2025
 const constants = {
-  HOST: 'https://alejandria-mernstackportfolio.onrender.com'
+  HOST: 'https://alejandria-mernstackportfolio.onrender.com/api'
 };
 
 export default constants;
